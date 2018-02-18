2acd535d5972e979f564c64494d066fc74247004
Rong <rayanlei@gmail.com>
2018-02-17 19:26:55 -0800

    add empty rails 4.0, ruby 2.1.2 app

---
+++ bookreview/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookreview::Application.config.secret_key_base = '9d346c3f0077225cf5a34e6fd934195575e1243570be48ab5e4e6d5da5d046596e6c6b411c26355a5db7ce7ab8a63ce055609eb8885c434f5d99b68de94ec988'
---
+++ bookreview/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookreview::Application.config.session_store :cookie_store, key: '_bookreview_session'
