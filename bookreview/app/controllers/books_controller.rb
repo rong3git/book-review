634f4c552533f76ffd3d39f99c2a456083a9df64
Rong <rayanlei@gmail.com>
2018-02-23 17:53:36 -0800

    category

--- bookreview/app/controllers/books_controller.rb
+++ bookreview/app/controllers/books_controller.rb
@@ -2,7 +2,12 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy ]
 
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -10,10 +15,13 @@ class BooksController < ApplicationController
 
   def new
     @book = current_user.books.build
+    @categories = Category.all.map {|c| [c.name, c.id] }
   end
 
   def create
     @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
+
     if @book.save
       redirect_to root_path
     else
@@ -22,10 +30,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @categories = Category.all.map {|c| [c.name, c.id] }
 
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -40,7 +50,7 @@ class BooksController < ApplicationController
 
   private
     def book_params
-      params.require(:book).permit(:title, :description, :author)
+      params.require(:book).permit(:title, :description, :author, :category_id)
     end
 
     def find_book
--- bookreview/db/schema.rb
+++ bookreview/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223035522) do
+ActiveRecord::Schema.define(version: 20180223042439) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180223035522) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "category_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
